7a0c26bc3960bd5f5d060681adf6530590a399bf
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-08 14:29:21 +0100

    Style input

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -19,6 +19,27 @@ const styles = StyleSheet.create({
 	inputContainer: {
 		flex: 1,
 		marginTop: 100,
-		padding: 16
+		marginHorizontal: 24,
+		padding: 16,
+		backgroundColor: '#72063c',
+		borderRadius: 8,
+		// Shadow on Android
+		elevation: 15,
+		// Shadow on IOS
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25
+	},
+	numberInput: {
+		height: 50,
+		width: 50,
+		fontSize: 32,
+		borderBottomColor: '#ddb52f',
+		borderBottomWidth: 2,
+		color: '#ddb52f',
+		marginVertical: 8,
+		fontWeight: 'bold',
+		textAlign: 'center'
 	}
 })
