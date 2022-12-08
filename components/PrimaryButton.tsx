92f17fde41898ac64cc187a091ba94cbda37a1fe
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-08 15:29:36 +0100

    Style buttons

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,17 +1,51 @@
-import { View, Pressable, Text } from 'react-native'
+import { View, Pressable, Text, StyleSheet } from 'react-native'
 
 interface PrimaryButtonProps {
 	children: React.ReactNode
 }
 
 function PrimaryButton({ children }: PrimaryButtonProps): JSX.Element {
+	const pressHandler = () => {
+		console.log('Pressed')
+	}
+
 	return (
-		<Pressable>
-			<View>
-				<Text>{children}</Text>
-			</View>
-		</Pressable>
+		<View style={styles.buttonOuterContainer}>
+			<Pressable
+				style={({ pressed }) =>
+					pressed
+						? [styles.pressed, styles.buttonInnerContainer]
+						: styles.buttonInnerContainer
+				}
+				onPress={pressHandler}
+				android_ripple={{ color: '#640233' }}
+			>
+				<Text style={styles.buttonText}>{children}</Text>
+			</Pressable>
+		</View>
 	)
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+	// Fixes common ripple issue
+	buttonOuterContainer: {
+		borderRadius: 28,
+		margin: 4,
+		overflow: 'hidden'
+	},
+	buttonInnerContainer: {
+		backgroundColor: '#72063c',
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		elevation: 2
+	},
+	buttonText: {
+		color: '#fff',
+		textAlign: 'center'
+	},
+	pressed: {
+		opacity: 0.75
+	}
+})
