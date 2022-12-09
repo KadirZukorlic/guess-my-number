de6ad4661514897caaf39de2707a66c3bf32b64d
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-09 16:26:32 +0100

    Add background image under gradient

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
+import { StyleSheet, ImageBackground } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
+	return (
 		<LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+			<ImageBackground
+				source={require('./assets/images/background.png')}
+				resizeMode="cover"
+				style={styles.rootScreen}
+				imageStyle={styles.backgrounImage}
+			>
+				<StartGameScreen />
+			</ImageBackground>
 		</LinearGradient>
+	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1
+	},
+	backgrounImage: {
+		opacity: 0.15
+	}
+})
