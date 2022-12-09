6af68da110534ad1b18bbda73d58cd7cabe4bb6e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-09 22:54:33 +0100

    Add validation on confirm button

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,7 +1,29 @@
-import { View, TextInput, StyleSheet } from 'react-native'
+import { useState } from 'react'
+import { View, TextInput, StyleSheet, Alert } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
 
 const StartGameScreen = () => {
+	const [enteredNumber, setEnteredNumber] = useState<string>('')
+
+	const numberInputHandler = (enteredText: string) => {
+		setEnteredNumber(enteredText)
+	}
+
+	const resetInputHandler = () => setEnteredNumber('')
+
+	const confirmInputHandler = (): void => {
+		const number = parseInt(enteredNumber)
+		if (isNaN(number) || (number <= 0 || number > 99)) {
+			Alert.alert(
+				'Invalid number!',
+				'Number has to be a number between 1 and 99.',
+				[{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+			)
+			return
+		}
+		console.log('Valid number')
+	}
+
 	return (
 		<View style={styles.inputContainer}>
 			<TextInput
@@ -10,13 +32,15 @@ const StartGameScreen = () => {
 				keyboardType="number-pad"
 				autoCapitalize="none"
 				autoCorrect={false}
+				value={enteredNumber}
+				onChangeText={numberInputHandler}
 			/>
 			<View style={styles.buttonsContainer}>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Reset</PrimaryButton>
+					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
 				</View>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Confirm</PrimaryButton>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
 				</View>
 			</View>
 		</View>
