ec70a3e5f8ea13ab2b1dc258ee3098150fb86ca4
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-11 19:08:10 +0100

    Add generate random integer function

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,6 +1,20 @@
 import { View, Text, StyleSheet } from 'react-native'
 import Title from '../components/Title'
 
+const generateRandomBetween = (
+	min: number,
+	max: number,
+	exclude: number
+): number => {
+	const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+	if (rndNum === exclude) {
+		return generateRandomBetween(min, max, exclude)
+	} else {
+		return rndNum
+	}
+}
+
 function GameScreen() {
 	return (
 		<View style={styles.screen}>
