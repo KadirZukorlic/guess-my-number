cc5a1eca83b49c3242967933f7e56158588882c6
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-12 03:00:38 +0100

    Add logic for higher and lower hint buttons

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Alert } from 'react-native'
 import Title from '../components/ui/Title'
 import NumberContainer from '../components/game/NumberContainer'
+import PrimaryButton from '../components/ui/PrimaryButton'
 
 const generateRandomBetween = (
 	min: number,
@@ -17,9 +18,32 @@ const generateRandomBetween = (
 	}
 }
 
+let minNumber: number = 1
+let maxNumber: number = 100
+
 function GameScreen({ userNumber }: { userNumber: number }) {
-	const initialGuess = generateRandomBetween(1, 100, userNumber)
+	const initialGuess = generateRandomBetween(minNumber, maxNumber, userNumber)
 	const [currentGuess, setCurrentGuess] = useState<number>(initialGuess)
+
+	const nextGuessHandler = (direction: 'lower' | 'higher'): void => {
+		if (
+			(direction == 'lower' && currentGuess < userNumber) ||
+			(direction == 'higher' && currentGuess > userNumber)
+		) {
+			Alert.alert("Don't lie!", 'You know that this is wrong...', [
+				{ text: 'Sorry', style: 'cancel' }
+			])
+			return
+		}
+
+		if (direction == 'lower') {
+			maxNumber = currentGuess
+		} else {
+			minNumber = currentGuess + 1
+		}
+		const newRndNum = generateRandomBetween(minNumber, maxNumber, currentGuess)
+		setCurrentGuess(newRndNum)
+	}
 
 	return (
 		<View style={styles.screen}>
@@ -27,7 +51,14 @@ function GameScreen({ userNumber }: { userNumber: number }) {
 			<NumberContainer>{currentGuess}</NumberContainer>
 			<View>
 				<Text>Higher or lower?</Text>
-				{/* + - */}
+				<View>
+					<PrimaryButton onPress={() => nextGuessHandler('lower')}>
+						-
+					</PrimaryButton>
+					<PrimaryButton onPress={() => nextGuessHandler('higher')}>
+						+
+					</PrimaryButton>
+				</View>
 			</View>
 			{/* <View>LOG ROUNDS</View> */}
 		</View>
