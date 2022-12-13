e3fcd1c431ef43cc6cbb133d357fd212e962c58d
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-13 17:11:50 +0100

    Use InstructionText component

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { View, Text, TextInput, StyleSheet, Alert } from 'react-native'
 import Card from '../components/ui/Card'
+import InstructionText from '../components/ui/InstructionText'
 import PrimaryButton from '../components/ui/PrimaryButton'
 import Title from '../components/ui/Title'
 import Colors from '../constants/colors'
@@ -35,7 +36,7 @@ const StartGameScreen = ({ onPickNumber }: Props) => {
 		<View style={styles.rootContainer}>
 			<Title>Guess My Number</Title>
 			<Card>
-				<Text style={styles.instructionText}>Enter a number</Text>
+				<InstructionText>Enter a number</InstructionText>
 				<TextInput
 					style={styles.numberInput}
 					maxLength={2}
@@ -66,10 +67,7 @@ const styles = StyleSheet.create({
 		marginTop: 100,
 		alignItems: 'center'
 	},
-	instructionText: {
-		color: Colors.accent500,
-		fontSize: 24
-	},
+
 	numberInput: {
 		height: 50,
 		width: 50,
