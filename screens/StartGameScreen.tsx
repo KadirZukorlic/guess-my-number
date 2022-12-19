b8b9c67bcc6c71c7af0a080bde5aa03b735b258b
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-12-19 14:20:03 +0100

    Optimize ui when rotated

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-import { View, Text, TextInput, StyleSheet, Alert } from 'react-native'
+import {
+	View,
+	TextInput,
+	StyleSheet,
+	Alert,
+	useWindowDimensions
+} from 'react-native'
 import Card from '../components/ui/Card'
 import InstructionText from '../components/ui/InstructionText'
 import PrimaryButton from '../components/ui/PrimaryButton'
@@ -12,6 +18,8 @@ interface Props {
 
 const StartGameScreen = ({ onPickNumber }: Props) => {
 	const [enteredNumber, setEnteredNumber] = useState<string>('')
+
+	const { width, height } = useWindowDimensions()
 
 	const numberInputHandler = (enteredText: string) => {
 		setEnteredNumber(enteredText)
@@ -32,8 +40,10 @@ const StartGameScreen = ({ onPickNumber }: Props) => {
 		onPickNumber(number)
 	}
 
+	const marginTopDistance = height < 380 ? 30 : 100
+
 	return (
-		<View style={styles.rootContainer}>
+		<View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
 			<Title>Guess My Number</Title>
 			<Card>
 				<InstructionText>Enter a number</InstructionText>
@@ -61,10 +71,13 @@ const StartGameScreen = ({ onPickNumber }: Props) => {
 
 export default StartGameScreen
 
+// Comment this out in favor of useWindowDimensions hook
+// const deviceHeight = Dimensions.get('window').height
+
 const styles = StyleSheet.create({
 	rootContainer: {
 		flex: 1,
-		marginTop: 100,
+		// marginTop: deviceHeight < 380 ? 30 : 100,
 		alignItems: 'center'
 	},
 
